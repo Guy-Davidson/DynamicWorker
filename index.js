4d1dc9a40fd4620beeb62e8fc6b7a297d0740e00
Guy Davidson <guydav12@gmail.com>
2022-05-30 11:24:54 +0300

    takejob

--- index.js
+++ index.js
@@ -1,58 +1,24 @@
 const fs = require('fs')
-const express = require('express');
 const axios = require('axios')
-// const fileUpload = require('express-fileupload')
-// const { v4: uuidv4 } = require('uuid');
-
-const app = express();
-// app.use(express.urlencoded({extended: true}))
-// app.use(express.json())
-// app.use(fileUpload())
-
-// const inQueue = []
-// initAutoScaler(inQueue)
-// const outQueue = []
-
-app.get('/', (req, res) => {    
-    res.send(`<h1>Cloud Computing Dynamic System - A Worker Node!</h1>`)
-})
-
-app.get('/test', (req, res) => {    
-    res.send(`test ok`)
-})
 
 const takeJob = async () => {    
     let hasWork = true
+    let ipsArr = fs.readFileSync('../ips.txt', 'utf8').split(',').map(ip => ip.split(':')[1])
     while(hasWork) {
-        // hasWork = false
-        for(const ip of loadIps()) {
+        hasWork = false
+        for(const ip of ipsArr) {
             console.log(ip);
             await axios
-                .get(`${ip}:5000/test`)
+                .get(`http://${ip}:5000/dequeue`) 
                 .then(res => console.log(res.data))
+                .catch(e => console.log("error"))
         }         
         await sleep(5 * 1000)
     }
 }
 
-const loadIps = () => {        
-    fs.readFileSync('../ips.txt', 'utf8' , (err, data) => {
-        if (err) res.send(err)
-        else {
-            return data.split(',').map(ip => ip.split(':')[1])
-        }             
-      })
-}
+takeJob()
 
 const sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-
-takeJob()
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)  
-}) 
