d96ddc2fc1cd4b8a846e2a56e627e17564632dd6
Guy Davidson <guydav12@gmail.com>
2022-05-30 14:58:57 +0300

    sh234

--- shutDown.js
+++ shutDown.js
@@ -1,10 +1,11 @@
+const fs = require('fs')
 const AWS = require('aws-sdk');
 AWS.config.region = 'eu-west-1'
 
 const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
 const shutDown = () => {
-    let workerId = fs.readFileSync('../workerId.txt', 'utf8')
+    let workerId = fs.readFileSync('../workerId.txt', 'utf8')    
     
     ec2.stopInstances({InstanceIds: [workerId]}, (err, data) => {
         if (err) console.log("Error", err) 
