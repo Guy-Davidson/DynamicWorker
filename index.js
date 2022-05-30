45a121e1f54d9a147dc338e67f90bcca62a1feb8
Guy Davidson <guydav12@gmail.com>
2022-05-30 13:33:51 +0300

    shutdown 1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const axios = require('axios')
 const sha512 = require('js-sha512')
+const shutDown = require('./shutDown')
 
 // max num a work will fail to get a job before shutting down. 
 const MAX_ATTEMPTS = 5
@@ -16,27 +17,43 @@ const takeJob = async () => {
         for(const ip of ipsArr) {            
             await axios
                 .get(`http://${ip}:5000/dequeue`) 
-                .then(res => {
+                .then(async (res) => {
                     
                     if(res.data !== "empty") {                        
                         hasWork = true
                         attempts = 0
 
                         let output = sha512(res.data.binaryDataBuffer.data)                        
-                        for(let i = 0; i < parseInt(res.data.iterations); i++) output = sha512(output)                        
-                        console.log(output)
-
+                        for(let i = 0; i < parseInt(res.data.iterations); i++) output = sha512(output)                                                
+                        await sendJob({ id: res.data.id, output: output}, ip)
                     }
                 })
                 .catch(e => console.log("error"))
         } 
         
         if(!hasWork) attempts += 1
-        if(attempts >= MAX_ATTEMPTS) // shut down()
+        if(attempts >= MAX_ATTEMPTS) {
+            console.log("should shut down");
+            shutDown()
+            break
+        }
+
         await sleep(SLEEP_DUR * 1000)
     }
+}
 
-    // shutDown()
+const sendJob = async (output, ip) => {
+    let confirmed = false
+    while(!confirmed) {
+        await axios
+            .put(`http://${ip}:5000/enqueueCompleted`, output)
+            .then(res => {
+                if(res.data === "ok") confirmed = true
+                console.log("successfully sent job!");
+            })
+            .catch(e => console.log(e))
+        await sleep(1 * 1000)
+    }
 }
 
 takeJob()
